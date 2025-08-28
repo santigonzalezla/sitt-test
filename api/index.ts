02b3acef0d9b9a6a60b0e1c504330f2be08e8b6b
Santiago Gonzalez <santigonzalezla@gmail.com>
2025-08-28 17:28:10 -0500

    prod preparation

---
+++ api/index.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import router from "../src/router/index";
+import environment from "../src/config/index";
+import helmet from "helmet";
+import limiter from "../src/lib/express_rate_limit";
+import {connectToDatabase} from "../src/database/mongoose";
+import {logger} from "../src/lib/winston";
+import {errorHandler, notFoundHandler} from "../src/middlewares/errorHandler";
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(limiter);
+app.use('/', router());
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+// Conectar a la base de datos una vez
+let isConnected = false;
+
+const connectDB = async () => {
+    if (!isConnected) {
+        await connectToDatabase();
+        isConnected = true;
+        logger.info('Database connected for serverless function');
+    }
+};
+
+// Export para Vercel
+export default async (req: any, res: any) => {
+    await connectDB();
+    app(req, res);
+};
